591a02fa9c90e0f61d9b1f16bc08f9efbeb26246
Suhana Kaura <123460149+suhanakaura@users.noreply.github.com>
2024-08-22 13:05:12 +0530

    Add files via upload

---
+++ Authorisation_and_Authetication/model/user.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/authtestapp")
+
+const userSchema = mongoose.Schema({
+    name:String,
+    email:String,
+    password:String,
+    age:Number
+})
+
+module.exports = mongoose.model("user",userSchema)
